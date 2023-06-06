522c5d6a20b9ee49b8c19bdabec6506b248a7fb0
RedRe4per <s3582474@gmail.com>
2023-06-06 20:21:51 +1000

    feat: init

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("renders a heading", () => {});
+});
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <main className="bg-gray-800 text-white h-[100vh] p-12">
+        <h1>Hello Derek!</h1>
+      </main>
+    </>
+  );
+}
