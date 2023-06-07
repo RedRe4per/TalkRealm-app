bc25ceabe7dc25513db6b0b5da3489d89290ca37
RedRe4per <s3582474@gmail.com>
2023-06-08 02:47:31 +1000

    feat: hero component init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <>
-      <main className="bg-primary h-[100vh] p-12">
-        <h1>Hello Derek!</h1>
+      <main className="bg-primary">
+        <Hero/>
       </main>
     </>
   );
