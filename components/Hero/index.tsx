475637adb40a4d9907e33bbd59b718ea70cb91e9
RedRe4per <s3582474@gmail.com>
2023-06-13 09:34:49 +1000

    feat: hero section

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -3,66 +3,47 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <div className="bg-primary-300">
-      <div className="relative isolate pt-14">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 pointer-events-none"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#F78282] to-[#D4AF07] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
-        </div>
-        <div className="py-24 sm:py-32 lg:pb-40">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto max-w-2xl text-center">
-              <h1 className="text-4xl font-bold tracking-tight text-secondary sm:text-6xl">
-                Bridging Distances Through Live Chat.
-              </h1>
-              <p className="mt-6 text-lg leading-8 text-secondary-300">
-                Break the barriers of distance with our live chat feature.
-                Connect, communicate, and create memories in real-time.
-              </p>
-              <div className="mt-10 flex items-center justify-center gap-x-10">
-                <Link
-                  href="#"
-                  className="rounded-md bg-quaternary px-3.5 py-2.5 text-text-3-standard font-semibold text-secondary shadow-sm hover:bg-quaternary-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-quaternary-200"
-                >
-                  Get started
-                </Link>
-                <a
-                  href="#"
-                  className="text-text-3-standard font-semibold leading-6 text-secondary"
-                >
-                  Learn more <span aria-hidden="true">→</span>
-                </a>
-              </div>
+      <div className="mx-auto max-w-7xl"> 
+        <div className="relative isolate overflow-hidden rounded-lg bg-primary-300 px-6 pt-16 sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <svg
+            viewBox="0 0 1024 1024"
+            className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+            aria-hidden="true"
+          >
+            <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+          </svg>
+          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+            <h2 className="text-heading-large-4 tracking-tight text-secondary-100 sm:text-heading-large-5">
+            Enhance Your Communication.
+              <br />
+              Connect with Our App Today.
+            </h2>
+            <p className="mt-6 text-text-3-standard leading-8 text-secondary">
+            Revolutionize your virtual meetings with high-quality video calls. Start your journey with us today.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+              <a
+                href="#"
+                className="rounded-md bg-quaternary px-3.5 py-2.5 text-text-2 font-semibold text-secondary-100 shadow-sm hover:bg-quaternary-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                Create new room
+              </a>
+              <a href="#" className="text-heading-small-8 leading-6 text-white">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
             </div>
+          </div>
+          <div className="relative mt-16 h-80 lg:mt-8">
             <img
-              src="https://my-notes-picture-derek-zhu.s3.ap-southeast-2.amazonaws.com/web-development/videochat1.jpg"
+              className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
               alt="App screenshot"
-              width={2432}
-              height={1442}
-              className="mt-16 rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10 sm:mt-24"
+              width={1824}
+              height={1080}
             />
           </div>
         </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
-        </div>
-      </div>
+       </div>
     </div>
-  );
+  )
 }
