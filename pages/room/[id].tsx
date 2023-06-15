fba3f969be2fa2f0ec24442d4a1c3725b00c22f6
RedRe4per <s3582474@gmail.com>
2023-06-15 13:33:49 +1000

    feat: create and receive offer and answer

--- pages/room/[id].tsx
+++ pages/room/[id].tsx
@@ -12,24 +12,82 @@ export default function Room(roomInfo: Props) {
   const [camera, setCamera] = useState(false);
   const [shareScreen, setShareScreen] = useState(false);
   const [socket, setSocket] = useState<Socket | null>(null);
+  const [pc, setPc] = useState<RTCPeerConnection | null>(null);
 
   useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setPc(new RTCPeerConnection());
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined' && pc) {
+  
+  //     pc.onicecandidate = (event) => {
+  //       if (event.candidate && socket) {
+  //         socket.emit('ice-candidate', event.candidate);
+  //       }
+  //     };
+  
+  //     setPc(pc);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    if (pc === null) {
+      return;
+    }
+
     const socketIo = io(`${process.env.NEXT_PUBLIC_SERVER_ADDRESS}`);
 
     socketIo.on("message", (message) => {
       console.log(message);
     });
 
+    
+      socketIo.on('offer', async (offer) => {
+        console.log('Offer received');
+        await pc.setRemoteDescription(offer);
+        const answer = await pc.createAnswer();
+        await pc.setLocalDescription(answer);
+        socketIo.emit('answer', answer);
+      });
+  
+      socketIo.on('answer', async (answer) => {
+        console.log('Answer received');
+        await pc.setRemoteDescription(answer);
+      });
+
+      // socketIo.on('ice-candidate', async (candidate) => {
+      //   if (pc) {
+      //     try {
+      //       await pc.addIceCandidate(candidate);
+      //     } catch (e) {
+      //       console.error('Error adding received ice candidate', e);
+      //     }
+      //   }
+      // });
+    
+
     setSocket(socketIo);
 
     return () => {
       socketIo.disconnect();
     };
-  }, []);
+  }, [pc]);
 
   const handleMessage = () => {
     if (socket) {
       socket.emit("message", "Hello from client!");
+    }
+  };
+
+  const handleCreateOffer = async () => {
+    if (socket && pc) {
+      console.log('Creating offer');
+      const offer = await pc.createOffer();
+      await pc.setLocalDescription(offer);
+      socket.emit("offer", offer);
     }
   };
 
@@ -47,6 +105,9 @@ export default function Room(roomInfo: Props) {
       <section>
         <button className="text-quaternary" onClick={handleMessage}>
           send message
+        </button>
+        <button className="text-green-500" onClick={handleCreateOffer}>
+          create offer 
         </button>
         <VideoChat muted={muted} camera={camera} shareScreen={shareScreen} />
       </section>
